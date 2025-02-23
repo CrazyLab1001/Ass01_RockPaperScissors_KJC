6584f6c5f8eef38b3e890cf209766736d6656909
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-02-23 12:00:38 -0500

    Troubleshooting complete and issue resolved.

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,13 @@ public class Main {
         System.out.println("Player 1, what's your move? [R, P, or S?]");
         Scanner in = new Scanner(System.in);
         do {
-            trash = in.nextLine().toUpperCase();
-            if (trash.equals("R") || trash.equals("P") || trash.equals("S")) {
-                playerOne = in.nextLine();
-                playerOne = playerOne.toUpperCase();
-            } else {
+            playerOne = in.nextLine();
+            playerOne = playerOne.toUpperCase();
+            if (!playerOne.equals("R") || !playerOne.equals("P") || !playerOne.equals("S")) {
+                System.out.println("Valid input! Moving on to Player 2!");
+                        }
+            else {
+                trash = playerOne;
                 System.out.println("Oops! " + trash + " is not a valid input. Try R, P, or S!");
             }
         } while (!(playerOne.equals("R") || playerOne.equals("P") || playerOne.equals("S"))); // validate Player One's move
